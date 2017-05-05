cd1a77f5f44fb72e289612808f4606d7de23c949
thomas.schnuerer <thomas.schnuerer@men>
2017-05-05 14:48:14 +0200

    added GPL license notification under every MEN document header

--- memdump.c
+++ memdump.c
@@ -52,7 +52,21 @@
  *
  *---------------------------------------------------------------------------
  * Copyright (c) 1998 MEN Mikro Elektronik GmbH. All rights reserved.
- ****************************************************************************/
+ ******************************************************************************/
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 char UTL_memdump_RCSid[]="$Id: memdump.c,v 1.11 2009/07/22 13:26:59 dpfeuffer Exp $";
 
--- permit.c
+++ permit.c
@@ -28,8 +28,22 @@
  * Initial Revision
  *
  *---------------------------------------------------------------------------
- * (c) Copyright 2009 by MEN mikro elektronik GmbH, Nuernberg, Germany
- ****************************************************************************/
+ * (c) Copyright 2009 by MEN mikro elektronik GmbH, Nuremberg, Germany
+ ******************************************************************************/
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #if defined(OS9000)
 static const char RCSid[]="$Header: /dd2/CVSR/COM/LIBSRC/USR_UTL/COM/permit.c,v 1.3 2014/10/29 12:03:39 channoyer Exp $";
 
