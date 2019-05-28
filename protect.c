5063d1b8b1516af4262d913c95f880f92d2741bc
KonradJPE <ktomasik@jpembedded.eu>
2019-05-28 18:45:11 +0200

    Add LGPL license
    Issue 'MDIS license clearing'
    https://github.com/MEN-Mikro-Elektronik/13MD05-90/issues/116

--- protect.c
+++ protect.c
@@ -18,18 +18,18 @@
  * Copyright (c) 2009-2019, MEN Mikro Elektronik GmbH
  ******************************************************************************/
 /*
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 #if defined(OS9000)
 static const char RCSid[]="$Header: /dd2/CVSR/COM/LIBSRC/USR_UTL/COM/protect.c,v 1.3 2014/10/29 12:04:43 channoyer Exp $";
